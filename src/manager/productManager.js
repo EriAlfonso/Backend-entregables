8ed396507156247ffcce7eb6fab06c06310eaa61
Eri Alfonso <thecheese.gw2@gmail.com>
2023-06-29 10:50:53 -0300

    remplazo de archivos y organizacion

--- src/manager/productManager.js
+++ src/manager/productManager.js
@@ -1,12 +1,11 @@
-import fs from "fs"
+import fs from "fs";
 
 // creamos la clase ProductManager y la exportamos
 export default class productManager {
     constructor(path) {
-        this.path = path
-        this.format = 'utf-8'
+        this.path = path;
+        this.format = "utf-8";
     }
-
 
     // muestra todos los productos en el array
     getProducts = async () => {
@@ -14,7 +13,7 @@ export default class productManager {
             const content = await fs.promises.readFile(this.path, this.format);
             return JSON.parse(content.toString());
         } catch (error) {
-            console.log('Error: Product List Empty:', error);
+            console.log("Error: Product List Empty:", error);
             return [];
         }
     };
@@ -23,14 +22,14 @@ export default class productManager {
     getNewId = async () => {
         const productList = await this.getProducts();
         let count = 0;
-  productList.forEach(product => {
-    if (product.id > count) {
-      count = product.id;
-    }
-  });
-  const newCount = ++count;
-return newCount;
-};
+        productList.forEach((product) => {
+            if (product.id > count) {
+                count = product.id;
+            }
+        });
+        const newCount = ++count;
+        return newCount;
+    };
 
     // funcion para agregar producto
     addProduct = async (title, description, price, thumbnail, stock, code) => {
@@ -39,7 +38,7 @@ return newCount;
         }
         const productList = await this.getProducts();
         // codigo para impedir la repeticion de la variable "code"
-        const codeExists = productList.find(product => product.code === code);
+        const codeExists = productList.find((product) => product.code === code);
         if (codeExists) {
             return console.log(`Error: Product with code ${code} already exists.`);
         }
@@ -62,21 +61,26 @@ return newCount;
     // buscador de producto por id
     getProductById = async (id) => {
         const productList = await this.getProducts();
-        let product = productList.find(product => product.id === id)
+        let product = productList.find((product) => product.id === id);
         if (product) {
             // modificamos esto para que devuelva un objeto y no un string como antes
             return product;
-        }
-
-        else {
+        } else {
             // en caso de no encontrar el codigo
-            return `Code: ${id} Not Found`;
+            return `Id: ${id} Not Found`;
         }
-    }
-
+    };
 
     // actualiza el producto usando su id para buscarlo
-    updateProduct = async (id, title, description, price, thumbnail, stock, code) => {
+    updateProduct = async (
+        id,
+        title,
+        description,
+        price,
+        thumbnail,
+        stock,
+        code
+    ) => {
         const update = {
             id,
             title,
@@ -97,7 +101,7 @@ return newCount;
         productList[findID] = updatedProduct;
         await fs.promises.writeFile(this.path, JSON.stringify(productList));
         console.log(`Product with Id: ${id} has been updated.`);
-    }
+    };
 
     // funcion para borra producto por id
     deleteProduct = async (id) => {
@@ -112,21 +116,9 @@ return newCount;
     };
 }
 
-
 // test
-async function run() {
-    const test = new productManager('./product.json')
-    // await test.addProduct("juego 1", "un juego de mesa infantil", 2300, "...", 24, "I")
-    // await test.addProduct("juego 2", "un juego de mesa infantil", 2400, "...", 24, "II")
-    // await test.addProduct("juego 3", "un juego de mesa infantil", 2250, "...", 24, "III")
-    // await test.addProduct("juego 4", "un juego de mesa infantil", 2100, "...", 24, "IV")
-    // await test.addProduct("juego 5", "un juego de mesa infantil", 2000, "...", 24, "V")
-    // await test.addProduct("juego 6", "un juego de mesa infantil", 2150, "...", 24, "VI")
-    // await test.addProduct("juego 7", "un juego de mesa infantil", 2410, "...", 24, "VII")
-    // await test.addProduct("juego 8", "un juego de mesa infantil", 2320, "...", 24, "VIII")
-    // await test.addProduct("juego 9", "un juego de mesa infantil", 2300, "...", 24, "IX")
-    // await test.addProduct("juego 10", "un juego de mesa infantil", 2300, "...", 24, "X")
-    // console.log(await test.getProducts())
-}
+// async function run() {
+//     const test = new productManager("./product.json");
+// }
 
-run()
+// run();
---
+++ src/routes/Cart.router.js
@@ -0,0 +1,10 @@
+import {Router } from `express`
+const router = Router();// CART
+// Post para crear un nuevo carrito
+
+
+// Get para ver lista de productos
+
+// Post para agregar productos
+
+export default router
