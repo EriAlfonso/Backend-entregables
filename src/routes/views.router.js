a8980fc2db60f4f6a4517865b410dcdea6d18a88
Eri Alfonso <thecheese.gw2@gmail.com>
2023-07-15 00:55:13 -0300

    finishing touches

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,43 +1,43 @@
 import { Router } from "express";
-import productManager from '../manager/productManager.js';
+import productManager from "../manager/productManager.js";
 
-const router = Router()
+const router = Router();
 
 // import de product manager para incorporar productos
-const productManagerImport = new productManager('./product.json');
+const productManagerImport = new productManager("./product.json");
 
 // routers para los views
-router.get ('/', (req,res)=>{
-    res.render('index', {})
-})
+router.get("/", (req, res) => {
+    res.render("index", {});
+});
 
-router.get ('/home', async (req,res)=>{
+router.get("/home", async (req, res) => {
     const products = await productManagerImport.getProducts();
-    res.render('home', {products})
-})
+    res.render("home", { products });
+});
 
-router.get ('/realTimeProducts', async (req,res)=>{
+router.get("/realTimeProducts", async (req, res) => {
     const products = await productManagerImport.getProducts();
-    res.render('realTimeProducts', {products})
-})
+    res.render("realTimeProducts", { products });
+});
 
-router.get ('/add-products', async (req,res)=>{
-    res.render('form', {})
-})
+router.get("/add-products", async (req, res) => {
+    res.render("form", {});
+});
 
 router.post("/add-products", async (req, res) => {
-    const { title, description, price, thumbnail, category, stock, code } = req.body;
+    const { title, description, price, thumbnail, category, stock, code } =
+        req.body;
     const result = await productManagerImport.addProduct(
-      title,
-      description,
-      price,
-      thumbnail,
-      category,
-      stock,
-      code
+        title,
+        description,
+        price,
+        thumbnail,
+        category,
+        stock,
+        code
     );
-    console.log(req.body);
     res.redirect("/home");
-  });
+});
 
-export default router
+export default router;
