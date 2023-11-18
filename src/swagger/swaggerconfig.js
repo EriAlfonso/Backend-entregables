eaad5c8c3edd5e02f666a16213b86799fe951f6f
Eri Alfonso <thecheese.gw2@gmail.com>
2023-11-18 10:55:15 -0300

    swagger and logger

--- src/swagger/swaggerconfig.js
+++ src/swagger/swaggerconfig.js
@@ -1,6 +1,6 @@
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import __dirname from "../utils";
+import __dirname from "../utils.js";
 const swaggerOptions = {
     definition: {
         openapi:"3.0.1",
